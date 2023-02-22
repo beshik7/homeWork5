166d02afb3cf9ad4d2947df3cde024ae075ca1ca
beshik7 <tommypdx7@gmail.com>
2023-02-22 09:41:35 -0800

    Master commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+ public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+                task1();
+            }
+            public static void task1() {
+                    System.out.println(" Задача 1 ");
+                System.out.print("Введите user-agent браузера: ");
+
+                int clientOS = 0; // объявляем переменную clientOS
+
+
+                // выводим сообщение в зависимости от значения переменной clientOS
+                if (clientOS == 0) {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                } else if (clientOS == 1) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Ваша операционная система не поддерживается");
+                }
+            }
+ }
+
